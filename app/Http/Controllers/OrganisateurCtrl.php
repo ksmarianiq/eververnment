4125e357f443e666578552a322102e05c534e744
Phoenixe12 <abiodoundamala@gmail.com>
2023-05-08 14:10:55 +0000

    lund 6 14:10

--- app/Http/Controllers/OrganisateurCtrl.php
+++ app/Http/Controllers/OrganisateurCtrl.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Organisateur;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class OrganisateurCtrl extends Controller
 {
@@ -11,9 +13,20 @@ class OrganisateurCtrl extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        if ($request->ajax()) {
+            $data = Organisateur::select('nomOrg','num1Org','num2Org','emailOrg','whatsappNum')->get();
+            return DataTables::of($data)->addIndexColumn()
+                ->addColumn('action', function($data){
+                    $button = '<button type="button" name="edit" id="'.$data->id.'" class="edit btn btn-primary btn-sm"> <i class="bi bi-pencil-square"></i>Edit</button>';
+                    $button .= '   <button type="button" name="edit" id="'.$data->id.'" class="delete btn btn-danger btn-sm"> <i class="bi bi-backspace-reverse-fill"></i> Delete</button>';
+                    return $button;
+                })
+                ->make(true);
+        }
+
+        return view('users');
     }
 
     /**
